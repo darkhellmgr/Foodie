ebf5c5dfcadabaf2f030f6a100b1bf343344d40b
suman <mgrsuman16@gmail.com>
2017-12-06 10:22:11 +0545

    importing carousel

--- resturant/models.py
+++ resturant/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Carousel(models.Model):
+	slider_image = models.ImageField(null = True, blank = True, upload_to = 'slider/%Y/%m/%d',)
+	display_title = models.CharField(max_length = 100,)
+	url = models.URLField(null=True, blank=True,)
+	display_content = models.TextField(null=True,blank=True,)
+
+	
--- resturant/views.py
+++ resturant/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from resturant.models import Carousel
 
 def index(request):
 	return render(request, 'base.html')
+
+def home(request):
+	query = Carousel.object.all()
+	context_dict = {'slider': query}
+	return render(request,'base.html', context_dict)
