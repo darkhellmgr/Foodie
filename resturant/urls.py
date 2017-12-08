0c3e099670fe61fc9c55daed740a320cb8b9b22c
suman <mgrsuman16@gmail.com>
2017-12-08 21:27:24 +0545

    footer added

--- resturant/urls.py
+++ resturant/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login
 
-urlpatterns =[ url(r'^$', views.index , name= "home"),
+urlpatterns =[ 
+	url(r'^$', views.index , name= "base"),
+	url(r'^contact/$', login, {'template_name':'sources/login.html'})
+
 ]
--- resturant/views.py
+++ resturant/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from resturant.models import Carousel
 
 def index(request):
-	return render(request, 'base.html')
-
-def home(request):
-	query = Carousel.object.all()
+	query = Carousel.objects.all()
 	context_dict = {'slider': query}
 	return render(request,'base.html', context_dict)
+	
+
